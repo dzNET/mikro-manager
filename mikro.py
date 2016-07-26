1cde74d5c0ba0c1db6b4a27ffa4b3033bd3dc509
dznet <kuchin.ea@gmail.com>
2016-07-27 00:23:47 +0300

    first demo refact

--- mikro.py
+++ mikro.py
@@ -26,21 +26,20 @@ try:
 		res = res[0][1]
 		
 		print 'Your board: %s\nUptime is: %s' % (res["=board-name"], res["=uptime"])
-		print "\n\nYou are in an interactive menu\n-\nExample for change SSID: 'set ssid new_ssid'\n-\nShow all command example: \
-		'help'\n-\nEnter 'quit' for Exit\n"
+		print "\n\nYou are in an interactive menu\n-\nExample for change SSID: 'set ssid new_ssid'\n-\nShow all command example: 'help'\
+		\n-\nEnter 'quit' for Exit\n"
 		
 		def menu():
 			while True:				
 				word = raw_input("\n\tYour choise: ").split()
-				#print word
 				if word[0] == "quit":
 					break
 				elif word[0] == "set":
 					if word[1] == "ssid" and len(word) > 2:
-						#res = mk.talk(["/interface/wireless/set", "=.id=wlan1", "=ssid=" + word[3]])
-						res = mk.talk(["/interface/wireless/print"])
-						res = res[0][1]
-						print "\nNew SSID are:", res["=ssid"]
+						res = mk.talk(["/interface/wireless/set", "=.id=wlan2", "=ssid=" + word[2]])
+						for key in mk.talk(["/interface/wireless/print"]):
+							if key[0] == "!re":
+								print '\nYour %s SSID are: %s' % (key[1]["=name"], key[1]["=ssid"])
 					elif word[1] == "limit":
 						print "\ntimeOut Menu"
 					else:
@@ -48,12 +47,14 @@ try:
 				elif word[0] == "print":
 					if word[1] == "res":
 						res = mk.talk(["/system/resource/print"])
-						#res = res[0][1]
-						print res
+						res = res[0][1]
+						print '\nBoard: %s\nUptime: %s\nCPU load: %s\nFree memory: %.2f Mb\nFree space: %.2f Mb\nFirmware: %s\n' %\
+						(res["=board-name"], res["=uptime"], res["=cpu-load"], float(res["=free-memory"])/1024/1024, \
+						float(res["=free-hdd-space"])/1024/1024, res["=version"])
 					elif word[1] == "ssid":
-						res = mk.talk(["/interface/wireless/print"])
-						#res = res[0][1]["=ssid"]
-						print "\nYour SSID are:", res
+						for key in mk.talk(["/interface/wireless/print"]):
+							if key[0] == "!re":
+								print '\nYour %s SSID are: %s' % (key[1]["=name"], key[1]["=ssid"])
 					elif word[1] == "limit":
 						print "\ntimeOut Menu"
 					elif word[1] == "users":
@@ -70,12 +71,12 @@ try:
 								(key[1]["=.id"], key[1]["=uptime"], key[1]["=mac-address"], key[1]["=address"],\
 								key[1]["=authorized"], key[1]["=host-dead-time"])
 				elif word[0] == "help":
-					print "\n# Setting\n\tset ssid new_ssid\n\tset limit 12:00:00  # 12 hours\n# Printing\n\tprint res\n\tprint \
-					ssid\n\tprint limit\n\tprint users\nhelp\nquit"
+					print "\n# Setting\n\tset ssid new_ssid\n\tset limit 12:00:00  # 12 hours\n# Printing\n\tprint res\n\tprint ssid\
+					\n\tprint limit\n\tprint users\nhelp\nquit"
 				else:
 					print "\nwrong ithem"
 		menu()
-		
+
 	else:
 		print ip, 'is down! Check router internet connection!\n'	
 except:
